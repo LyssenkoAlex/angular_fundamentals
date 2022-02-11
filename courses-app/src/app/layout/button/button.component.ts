4c1db828ce7f9e4fb9dc8ee5cc40e2b56b438a86
Alexandr_Lyssenko <ALexandr>
2022-02-11 17:19:23 +0600

    task_1

---
+++ courses-app/src/app/layout/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  template: '<button><ng-content></ng-content></button>'
+
+})
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
